aed00309d91e20a11def3ee8e84fbbd50fd94fe3
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-11-10 20:37:24 +0530

    bio updated

--- GUI/Bio.java
+++ GUI/Bio.java
@@ -1,9 +1,11 @@
+import java.awt.GridLayout;
 import java.awt.List;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
@@ -13,66 +15,83 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.data.xy.XYDataset;
 
 public class Bio extends JFrame{
-	JFreeChart ph,temp,co2;
-	ChartPanel phPanel,co2Panel,tempPanel;
-	JLabel l1,l2,l3,l4;
-	JTextField phValue,tempValue,co2Value;
+	JFreeChart ph,temp,co2,humidity;
+	ChartPanel phPanel,co2Panel,tempPanel,humidityPanel;
+	JLabel l1,l2,l3,l4,ll;
+	JTextField phValue,tempValue,co2Value,humidityValue;
 	List list;
 	JScrollPane values;
 	public Bio(String str){
 		super(str);
-		setSize(1200,700);
+		setSize(1300,700);
 		setLocationRelativeTo(this);
 		setResizable(false);
 		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 		setLayout(null);
 		
+		JPanel pan=new JPanel();
+		pan.setLayout(new GridLayout(0,2));
+		pan.setBounds(10,10,850,600);
+		
 		ph=ChartFactory.createXYLineChart("pH Plot", "Time", "pH", null);
 		phPanel=new ChartPanel(ph);
-		phPanel.setBounds(10,10,650,200);
 		
 		temp=ChartFactory.createXYLineChart("Temperature Plot", "Time", "Temperature", null);
 		tempPanel=new ChartPanel(temp);
-		tempPanel.setBounds(10,220,650,200);
 		
 		co2=ChartFactory.createXYLineChart("CO2 Plot", "Time", "CO2", null);
 		co2Panel=new ChartPanel(co2);
-		co2Panel.setBounds(10,430,650,200);
+		
+		humidity=ChartFactory.createXYLineChart("Humidity Plot", "Time", "Humidity", null);
+		humidityPanel=new ChartPanel(humidity);
 		
 		l1=new JLabel("Current pH Value:");
-		l1.setBounds(700,10,200,50);
+		l1.setBounds(900,10,200,50);
 		l2=new JLabel("Current Temperature Value:");
-		l2.setBounds(700,220,200,50);
+		l2.setBounds(900,160,200,50);
 		l3=new JLabel("Current CO2 Value:");
-		l3.setBounds(700,430,200,50);
-		l4=new JLabel("Values List:");
-		l4.setBounds(1000,10,100,50);
+		l3.setBounds(900,310,200,50);
+		l4=new JLabel("Current Humidity Value:");
+		l4.setBounds(900,460,200,50);
+		ll=new JLabel("Values List:");
+		ll.setBounds(1000,10,100,50);
 		
 		phValue=new JTextField();
-		phValue.setBounds(700,60,200,50);
+		phValue.setBounds(900,60,200,50);
 		phValue.setEditable(false);
 		tempValue=new JTextField();
-		tempValue.setBounds(700,270,200,50);
+		tempValue.setBounds(900,210,200,50);
 		tempValue.setEditable(false);
 		co2Value=new JTextField();
-		co2Value.setBounds(700,480,200,50);
+		co2Value.setBounds(900,360,200,50);
 		co2Value.setEditable(false);
+		humidityValue=new JTextField();
+		humidityValue.setEditable(false);
+		humidityValue.setBounds(900,510,200,50);
 		
 		list=new List();
 		values=new JScrollPane(list);
-		values.setBounds(1000,60,170,500);
+		values.setBounds(1110,60,170,500);
 		
-		add(phPanel);
-		add(tempPanel);
-		add(co2Panel);
+		pan.add(phPanel);
 		add(l1);
-		add(l2);
-		add(l3);
-		add(l4);
 		add(phValue);
+		pan.add(tempPanel);
+		add(l2);
 		add(tempValue);
+		pan.add(co2Panel);
+		add(l3);
 		add(co2Value);
+		add(ll);
+		add(l4);
+		add(humidityValue);
+		pan.add(humidityPanel);
 		add(values);
+		add(pan);
+		plot();
+	}
+	public void plot(){
+		
 	}
 	public static void main(String[] args){
 		new Bio("bio").setVisible(true);
